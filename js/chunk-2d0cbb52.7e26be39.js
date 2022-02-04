5995ce2bec4636dd1c80c129314424dabdc165e8
chih shao <sam125055869@gmail.com>
2022-02-04 17:29:57 +0800

    gh-pages

---
+++ js/chunk-2d0cbb52.7e26be39.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0cbb52"],{"4b8f":function(e,n,t){"use strict";t.r(n);var u=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("router-view")],1)},c=[],r=t("2877"),l={},i=Object(r["a"])(l,u,c,!1,null,null,null);n["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-2d0cbb52.7e26be39.js.map
